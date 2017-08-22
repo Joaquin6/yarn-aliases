ca794adb33d41227e386dd4e729e5ad3beb8c5d2
Joaquin Briceno <joaquin_briceno@yahoo.com>
2017-08-22 08:42:53 -0700

    updated strings

--- yarn-aliases.sh
+++ yarn-aliases.sh
@@ -1,40 +1,40 @@
 # Aliases
-alias y='yarn'
+alias y="yarn"
 
-alias yaa='yarn add'
-alias yaad='yarn add --dev'
+alias yaa="yarn add"
+alias yaad="yarn add --dev"
 
-alias yacc='yarn cache clean'
-alias yack='yarn check'
+alias yacc="yarn cache clean"
+alias yack="yarn check"
 
-alias yint='yarn init'
-alias yain='yarn info'
+alias yint="yarn init"
+alias yain="yarn info"
 alias yinst="yarn install"
 
 alias yga="yarn global add"
 
-alias yali='yarn licenses ls'
+alias yali="yarn licenses ls"
 alias yl="yarn link"
-alias yals='yarn ls'
-alias yaloi='yarn login'
-alias yaloo='yarn logout'
+alias yals="yarn ls"
+alias yaloi="yarn login"
+alias yaloo="yarn logout"
 
-alias yaod='yarn outdated'
+alias yaod="yarn outdated"
 
-alias yapa='yarn pack'
-alias yap='yarn publish'
+alias yapa="yarn pack"
+alias yap="yarn publish"
 
-alias yarm='yarn remove'
+alias yarm="yarn remove"
 alias yr="yarn run"
 alias yre="yarn run example"
 
-alias yasu='yarn self-update'
+alias yasu="yarn self-update"
 alias ys="yarn start"
 
 alias yt="yarn test"
 
 alias yu="yarn unlink"
-alias yau='yarn upgrade'
+alias yau="yarn upgrade"
 
 # Combinations
 alias yis="yarn install && yarn start"
