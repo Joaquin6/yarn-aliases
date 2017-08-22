22eb42a368f1453e4ca91a1424b08b43ff52bf88
Joaquin Briceno <joaquin_briceno@yahoo.com>
2017-08-22 08:41:36 -0700

    added aliases

--- yarn-aliases.sh
+++ yarn-aliases.sh
@@ -1,12 +1,41 @@
-alias yi="yarn install"
-alias ya="yarn add"
-alias yad="yarn add --dev"
+# Aliases
+alias y='yarn'
+
+alias yaa='yarn add'
+alias yaad='yarn add --dev'
+
+alias yacc='yarn cache clean'
+alias yack='yarn check'
+
+alias yint='yarn init'
+alias yain='yarn info'
+alias yinst="yarn install"
+
 alias yga="yarn global add"
+
+alias yali='yarn licenses ls'
+alias yl="yarn link"
+alias yals='yarn ls'
+alias yaloi='yarn login'
+alias yaloo='yarn logout'
+
+alias yaod='yarn outdated'
+
+alias yapa='yarn pack'
+alias yap='yarn publish'
+
+alias yarm='yarn remove'
 alias yr="yarn run"
 alias yre="yarn run example"
+
+alias yasu='yarn self-update'
 alias ys="yarn start"
-alias yis="yarn install && yarn start"
+
 alias yt="yarn test"
-alias yit="yarn install && yarn test"
-alias yl="yarn link"
+
 alias yu="yarn unlink"
+alias yau='yarn upgrade'
+
+# Combinations
+alias yis="yarn install && yarn start"
+alias yit="yarn install && yarn test"
